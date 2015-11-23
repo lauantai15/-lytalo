9bf488402f4698061e0468213de594f818d2a3e4
lauantai15 <dima.kokovkin@stud.pointcollege.fi>
2015-11-23 15:12:21 +0200

    Toteutettu lämpötilan asettaminen

---
+++ TiedonTallennus/TiedonTallennus/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TiedonTallennus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lämpötila = 0;
+            Console.WriteLine("Anna uusi lämpötila");
+            string syöte = Console.ReadLine();
+            lämpötila = int.Parse(syöte);
+            //tiedoston tallentaminen
+
+            string tiedosto = @"C:\Temp\Lämpötila.txt";
+            File.WriteAllText(tiedosto, lämpötila.ToString());
+            Console.WriteLine("Uusi lämpötila tallennettu.");
+            Console.ReadLine();
+        }
+    }
+}
