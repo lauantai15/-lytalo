b3cb5f2e68d0a8faea17853b5f6d9b2aae511d6b
lauantai15 <dima.kokovkin@stud.pointcollege.fi>
2016-01-07 22:34:11 +0200

    ensimmäinen versio

---
+++ OmaCarHarjoitus/OmaCarHarjoitus/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmaCarHarjoitus
+{
+   public class Car
+    {
+        public string Color { get; set; }
+        public string Model { get; set; }
+
+        private int maxSpeed;
+        public int MaxSpeed {
+
+            get
+            {
+                return maxSpeed;
+            }
+
+            set
+            {
+                if ((value > 0) && (value < 500))
+                {
+                    maxSpeed = value;
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException();
+                }
+            }
+                
+           }
+
+
+        public void StartEngine()
+        {
+
+        }
+        public void StopEngine()
+        {
+
+
+        }
+
+    }
+}
---
+++ OmaCarHarjoitus/OmaCarHarjoitus/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OmaCarHarjoitus
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            Car myCar = new Car();
+            Car chevy = new Car();
+
+            int maxSpeed = int.Parse(maxSpeedTextBox.Text);
+            myCar.Color = "Grey";
+            myCar.MaxSpeed = maxSpeed;
+          //  MessageBox.Show("Huippunopeus asetettu ominaisuuden kautta");
+            myCar.Model = "Corolla";
+            myCar.StartEngine();
+            ShowCarInfo(myCar);
+
+            chevy.Color = "Red";
+           chevy.MaxSpeed = 300;
+            chevy.Model = "Corvette";
+            chevy.StartEngine();
+            ShowCarInfo(chevy);
+
+            MessageBox.Show(chevy.ToString());
+
+        }
+
+        public void ShowCarInfo(Car c)
+        {
+            string message = "Model: " + c.Model + "\r\n" + "Color: " + c.Color + "\r\n" + "Max speed: " 
+                + c.MaxSpeed;
+            MessageBox.Show(message);
+        }
+
+    }
+}
